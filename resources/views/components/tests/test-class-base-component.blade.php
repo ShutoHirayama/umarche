0b1c13ebf2ef924df18ba9827f4f0dc45d50e0eb
ShutoHirayama <hyst773@gmail.com>
2023-05-19 18:31:51 +0900

    マルチログインエラー

--- resources/views/components/tests/test-class-base-component.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    クラスベースのコンポーネントです。
-    <div>{{ $classBaseMessage }}</div>
-    <div>{{ $defaultMessage }}</div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-</div>
