ab659c6cca53baf8e3e927c16ce8cc07cb7fa4e6
ShutoHirayana <hirayama@linew.co.jp>
2023-05-24 16:36:31 +0900

    不具合修正

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log; 
 
 class AuthenticatedSessionController extends Controller
 {
@@ -17,7 +18,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create()
     {
-        return view('owners.auth.login');
+        return view('owner.auth.login');
     }
 
     /**
@@ -32,6 +33,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        Log::debug('owner', $request->session()->all());
+        
         return redirect()->intended(RouteServiceProvider::OWNER_HOME);
     }
 
